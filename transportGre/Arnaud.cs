6c2e3de5b1a269a53e844afa9178f2d93b9ca3f8
Cecilia <ceciliapigeolet@gmail.com>
2018-06-20 14:43:50 +0200

    ajout nuGet njson.net pour exploiter les données json

---
+++ transportGre/Arnaud.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace transportGre
+{
+    class Arnaud
+    {
+        /*
+         *Obtenus grâce à http://json2csharp.com/
+         * on a passé l'url openData dans le convertiseur json to c#
+         */
+        public string id { get; set; }
+        public string name { get; set; }
+        public double lon { get; set; }
+        public double lat { get; set; }
+        public List<string> listArnauds { get; set; }
+    }
+}
--- transportGre/Program.cs
+++ transportGre/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using System.IO;
+using static System.Console;
 
 namespace transportGre
 {
@@ -12,25 +13,36 @@ namespace transportGre
     {
         static void Main(string[] args)
         {
-            WebRequest request = WebRequest.Create("http://data.metromobilite.fr/api/linesNear/json?x=5.709360123&y=45.176494599999984&dist=800&details=true");
-           
+            WebRequest request = WebRequest.Create("http://data.metromobilite.fr/api/linesNear/json?x=5.709360123&y=45.176494599999984&dist=200&details=true");
+
             // Get the response.  
             WebResponse response = request.GetResponse();
             // Display the status.  
             Console.WriteLine(((HttpWebResponse)response).StatusDescription);
-           
+
             // Get the stream containing content returned by the server.  
             Stream dataStream = response.GetResponseStream();
             // Open the stream using a StreamReader for easy access.  
             StreamReader reader = new StreamReader(dataStream);
             // Read the content.  
             string responseFromServer = reader.ReadToEnd();
-            // Display the content.  
-            Console.WriteLine(responseFromServer);
-            Console.Read();
+
+
+            // Display the content.
+            /*
+             * on fait un liste de la classe Arnaud
+             * que l'on converti grâce au nuget json.net pour pouvoir utiliser les objets
+             */
+            List<Arnaud> listArnauds = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Arnaud>>(responseFromServer);
+            foreach (Arnaud listArnaud in listArnauds)
+            {
+                WriteLine(listArnaud.name);
+            }
+
             // Clean up the streams and the response.  
             reader.Close();
             response.Close();
+            Read();
         }
     }
 }
