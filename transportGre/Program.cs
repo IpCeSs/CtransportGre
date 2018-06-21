e380179ad9cd2f140646da16d773c51e80a3d723
Cecilia <ceciliapigeolet@gmail.com>
2018-06-21 09:56:41 +0200

    ajout librairie fonctionnelle

--- transportGre/Program.cs
+++ transportGre/Program.cs
@@ -1,54 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Net;
-using System.IO;
+﻿using transportLibrairie;
 using static System.Console;
-
+using System;
 namespace transportGre
 {
     class Program
     {
         static void Main(string[] args)
         {
-            WebRequest request = WebRequest.Create("http://data.metromobilite.fr/api/linesNear/json?x=5.709360123&y=45.176494599999984&dist=1000&details=true");
 
-            // Get the response.  
-            WebResponse response = request.GetResponse();
-            // Display the status.  
-            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
-
-            // Get the stream containing content returned by the server.  
-            Stream dataStream = response.GetResponseStream();
-            // Open the stream using a StreamReader for easy access.  
-            StreamReader reader = new StreamReader(dataStream);
-            // Read the content.  
-            string responseFromServer = reader.ReadToEnd();
-
-
-            // Display the content.
-            /*
-             * on fait un liste de la classe Arnaud
-             * que l'on converti grâce au nuget json.net pour pouvoir utiliser les objets
-             * on itère dans la liste avec un foreach
-             */
-            List<Arnaud> ListArnauds = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Arnaud>>(responseFromServer);
-            /*
-             *On crée une liste qui groupe by nom (touts les memes noms sont mis ensemble, et on select 
-             * seulement le first de cette nouvelle liste
-             */
-            List<Arnaud> ArnaudSansDoublons = ListArnauds.GroupBy(n => n.Name).Select(g => g.First()).ToList();
-            foreach (Arnaud ListArnaud in ArnaudSansDoublons)
-            {
-                WriteLine($"arret : {ListArnaud.Name} ligne : {ListArnaud.Lines[0]} ");
-            }
-
-            // Clean up the streams and the response.  
-            reader.Close();
-            response.Close();
-            Read();
+            Lib.RequeteMetroLignes();
+           
         }
     }
 }
+
---
+++ transportLibrairie/Lib.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using static System.Console;
+
+namespace transportLibrairie
+{
+    public class Lib
+    {
+        /*
+         *Obtenus grâce à http://json2csharp.com/
+         * on a passé l'url openData dans le convertiseur json to c#
+         */
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public double Lon { get; set; }
+        public double Lat { get; set; }
+        public List<string> Lines { get; set; }
+
+
+        public static void RequeteMetroLignes()
+        {
+            WebRequest request = WebRequest.Create("http://data.metromobilite.fr/api/linesNear/json?x=5.709360123&y=45.176494599999984&dist=200&details=true");
+
+            // Get the response.  
+            WebResponse response = request.GetResponse();
+            // Display the status.  
+            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+
+            // Get the stream containing content returned by the server.  
+            Stream dataStream = response.GetResponseStream();
+            // Open the stream using a StreamReader for easy access.  
+            StreamReader reader = new StreamReader(dataStream);
+            // Read the content.  
+            string responseFromServer = reader.ReadToEnd();
+            // Clean up the streams and the response. 
+            reader.Close();
+            response.Close();
+
+            List<Lib> ListLib = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Lib>>(responseFromServer);
+            /*
+             *On crée une liste qui groupe by nom (touts les memes noms sont mis ensemble, et on select 
+             * seulement le first de cette nouvelle liste
+             */
+            List<Lib> LibSansDoublons = ListLib.GroupBy(n => n.Name).Select(g => g.First()).ToList();
+
+            // Display the content.
+            /*
+             * on fait un liste de la classe Arnaud
+             * que l'on converti grâce au nuget json.net pour pouvoir utiliser les objets
+             * on itère dans la liste avec un foreach
+             */
+            foreach (Lib Lib in LibSansDoublons)
+            {
+                WriteLine($"arret : {Lib.Name} ligne : {Lib.Lines[0]} ");
+            }
+            Read();
+        }
+    }
+}
