eb90b01b79fb489ff405e63b56954fb3df927438
Cecilia <ceciliapigeolet@gmail.com>
2018-06-28 13:40:19 +0200

    ajout commentaire Model vide

---
+++ MetroMVVM/Model/MetroModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetroMVVM.Model
+{
+    class MetroModel
+    {
+        /*
+         * Nous n'avons ici pas de model interne au projet.
+         * Le modèle se trouve dans notre transportLibrairie : Lignes.
+         *
+         */
+    }
+}
